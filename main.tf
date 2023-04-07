b64829cedf8d02442470a963050f3685d75fa893
Gus Jimenez <jimenezgustavoandres@gmail.com>
2023-04-07 18:34:18 -0400

    terraform lambda python deploy exmaple

---
+++ main.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "terraform_aws_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+
+# AIAM policy for logging from a lambda
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managinng aws lambda role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      },
+    ]
+  })
+}
+
+# Policy Attachment on the role
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+# Generate an archive from the content, a file, or a directory of files.
+
+# Archive a single file.
+data "archive_file" "zip_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/python"
+  output_path = "${path.module}/python/hello-python.zip"
+}
+
+# Create a lambda function
+# In terraform ${path.module} is the current directory.
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "${path.module}/python/hello-python.zip"
+  function_name = "Goose_Sample_Lambda_Function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "hello-python.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
+output "terraform_aws_role_output" {
+  value = aws_iam_role.lambda_role.name
+}
+
+output "terraform_aws_role_arn_output" {
+  value = aws_iam_role.lambda_role.arn
+}
+
+output "terraform_logging_arn__output" {
+  value = aws_iam_policy.iam_policy_for_lambda.arn
+}
